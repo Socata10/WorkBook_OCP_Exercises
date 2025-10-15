6e7d3626e8b1a3af49f163aaeb50e2b10e4dee02
Victor Socaci <Victor.Socaci+endava@endava.com>
2025-10-15 14:33:48 +0300

    Exercise 5 from chapter 4-5

---
+++ src/Chapter4_5/Ex5/Animal.java
@@ -0,0 +1,5 @@
+package Chapter4_5.Ex5;
+
+public abstract class Animal {
+    abstract void makeSound();
+}
---
+++ src/Chapter4_5/Ex5/Eagle.java
@@ -0,0 +1,8 @@
+package Chapter4_5.Ex5;
+
+public class Eagle extends Bird{
+    @Override
+    void makeSound() {
+        System.out.println("Screech");
+    }
+}
